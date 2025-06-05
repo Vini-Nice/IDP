5a1d741b952e8c89473cb55c1d9c826cdb1e998b
RS0usa1 <richardrrggts@gmail.com>
2025-06-05 14:36:11 -0300

    Options - 14:35 - 05/05/2025

--- trabalho/routes/avisosRotas.js
+++ trabalho/routes/avisosRotas.js
@@ -18,4 +18,14 @@ router.put('/:id', atualizarAvisoController);
 // Excluir um aviso
 router.delete('/:id', excluirAvisoController);
 
+router.options('/', (req, res) => {
+    res.setHeader('Allow', 'GET, POST, OPTIONS');
+    res.status(204).send();
+});
+
+router.options('/:id', (req, res) => {
+    res.setHeader('Allow', 'GET, PUT, DELETE, OPTIONS');
+    res.status(204).send();
+});
+
 export default router;
